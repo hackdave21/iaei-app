69bef888c2834c7e1d056a90429ce217695cda41
hackdave21 <davalawi77@gmail.com>
2026-04-18 17:15:35 +0000

    Pour deploy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\SetLocale::class,
         ]);
+
+        $middleware->trustProxies(at: '*');
         
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
